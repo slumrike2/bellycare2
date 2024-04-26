747c6c89812b9017e9a00cb46cf6800dd1ac773c
Luis Barreto <luis.barreto.marin@gmail.com>
2024-04-25 22:32:19 -0400

    add styles for patient home

--- BellyCare/ViewModels/PatientHomeViewModel.cs
+++ BellyCare/ViewModels/PatientHomeViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using Barreto.Exe.Maui.Services.Navigation;
 using Barreto.Exe.Maui.ViewModels;
 using BellyCare.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace BellyCare.ViewModels
 {
     public partial class PatientHomeViewModel : BaseViewModel, IEventfulViewModel
     {
+        [ObservableProperty]
+        string name;
+
         public PatientHomeViewModel(ISettingsService settings, INavigationService navigationService) : base(settings, navigationService)
         {
         }
@@ -19,6 +23,7 @@ namespace BellyCare.ViewModels
 
         public void OnAppearing()
         {
+            Name = settings.Patient.Names;
         }
 
         public void OnDisappearing()
